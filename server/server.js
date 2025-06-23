75b23b4b6ad29965d50cdac66e69e7557f138f3c
xsmartbartx <bw141105@gmail.com>
2025-06-23 20:31:23 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
+import { connect } from 'http2';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true,}));
+app.use(cors({credentials: true}));
 
 app.get('/', (req, res) => res.send('Welcome to the server!');
 
