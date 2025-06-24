68316165cf08fe1c50de575d2b449488c16abff2
xsmartbartx <bw141105@gmail.com>
2025-06-24 17:37:35 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import { connect } from 'http2';
 
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
-app.get('/', (req, res) => res.send('Welcome to the server!');
+app.get('/', (req, res) => res.send('Welcome to the server!'));
+app.use('/api/auth', authRouter);
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
