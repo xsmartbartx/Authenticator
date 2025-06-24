b1f3573ddc0a0b13d31f86ec4b6fa06781b9bb1c
xsmartbartx <bw141105@gmail.com>
2025-06-24 14:38:54 +0200

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import userModel from '../config/models/userModel.js';
 
 export const register = async (req, res) => {
 
@@ -12,7 +13,20 @@ export const register = async (req, res) => {
 
     try {
 
+        const existingUser = await userModel.findOne({email})
+
+        if (existingUser) {
+            return res.json({success: false, message: "User already exists!"})
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        });
+        await user.save();
 
     } catch (error) {
         res.json({success: false, message: error.message});
