da65dc36568a4fd463416da4d47f1502a01c02c7
xsmartbartx <bw141105@gmail.com>
2025-06-24 17:38:34 +0200

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { login, register } from '../controllers/authController';
+import { login, logout, register } from '../controllers/authController';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
+
+export default authRouter;
