093ef898c117517422b09061cd227d24a9e4790a
xsmartbartx <bw141105@gmail.com>
2025-06-24 14:04:40 +0200

    Create userModel.js

---
+++ server/config/models/userModel.js
@@ -0,0 +1,10 @@
+import { verify } from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verifyOtp: {type: String, default: ''},
+    verifyOtpExpiresAt: {type: Number, default: 0}
+})
