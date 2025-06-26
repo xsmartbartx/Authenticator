fc9a8f17bf5e77fb6c14c44847ece8a2fb2d2ebb
xsmartbartx <bw141105@gmail.com>
2025-06-26 20:51:28 +0200

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -167,3 +167,9 @@ export const verifyEmail = async (req, res) => {
         return res.json({success: false, message: error.message});
     }
 }
+
+export const isAuthenticated = async (req, res) => {
+    try {
+        return res.json({success: false, message: "User is authenticated!"});
+    }
+}
