4869540227c1fc84371a913c782d244b55954bb4
xsmartbartx <bw141105@gmail.com>
2025-06-26 20:54:18 +0200

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, register, sendVerifyOtp } from '../controllers/authController';
+import { login, logout, register, sendVerifyOtp, verifyEmail } from '../controllers/authController';
 import userAuth from '../middleware/userAuth';
 import { verify } from 'jsonwebtoken';
 
@@ -10,5 +10,6 @@ authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
 authRouter.post('verify-account', userAuth, verifyAccount);
+authRouter.post('/is-auth', userAuth, verifyEmail);
 
 export default authRouter;
