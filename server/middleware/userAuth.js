7e6bd395a794ecdf122a71cf57f153b0e199edff
xsmartbartx <bw141105@gmail.com>
2025-06-26 20:35:08 +0200

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -9,6 +9,14 @@ const userAuth = (req, res, next) => {
 
     try {
 
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(tokenDecode.id) {
+            req.body.userId = tokenDecode.id;
+        }else {
+            return res.status(401).json({ success: false, message: "Unauthorized access!" });
+        }
+
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
