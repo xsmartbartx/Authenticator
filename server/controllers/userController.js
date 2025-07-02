76b40337f4bb4f61b113d6454b3c5b59dde3315a
xsmartbartx <bw141105@gmail.com>
2025-07-02 16:12:42 +0200

    Create userController.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+import userModel from "../config/models/userModel";
+
+export const getUserData = (req, res) => {
+    try {
+        const {userId} = req.body;
+
+        const user = userModel.findOne({ _id: userId });
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+
+
+
+    } catch (error) {
+            res.json({ success: false, message: error.message });
+    }
+}
