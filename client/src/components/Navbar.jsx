e86d7b533c7085adddf806796a14630d1481d6b0
xsmartbartx <bw141105@gmail.com>
2025-09-17 15:26:12 +0200

    Create Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src="/logo.png" alt="Logo" style={{height: '50px'}} />
+    </div>
+  )
+}
+
+export default Navbar
