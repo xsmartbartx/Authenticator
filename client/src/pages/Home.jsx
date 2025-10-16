a1237ce9fb97ea1b2d88b519a5d7f48219ee1894
xsmartbartx <bw141105@gmail.com>
2025-10-16 22:29:46 +0200

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,8 @@ import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen
+    bg-[url("/bg_img.png")] bg-cover bg-center'>
       <Navbar />
       <Header />
     </div>
