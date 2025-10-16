3483981e34ce187b3a03d47e2d4d1096baccc6db
xsmartbartx <bw141105@gmail.com>
2025-10-16 21:13:18 +0200

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <img src={assets.header_img} alt="" className='w-36 h-36 roinded-full mb-6'/>
+        <h1>Witaj<img className='w-8 aspect-square' src={assets.hand_wave} alt=""/></h1>
+    </div>
   )
 }
 
