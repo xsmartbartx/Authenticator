7607e1df313d95d29bd3850468359459b1a6f7d2
xsmartbartx <bw141105@gmail.com>
2025-10-16 22:54:53 +0200

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Login = () => {
+
+    const [state, setState] = useState('Zapisz się')
+
   return (
-    <div>Logowanie</div>
+    <div>
+      <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5
+       w-28 sm:w-32 cursor-pointer'/>
+       <div>
+        <h2 className='text-3x1 sm:text-5x1 font-semibold mb-4'>Zaloguj się do swojego konta</h1>
+       </div>
+    </div>
   )
 }
 
