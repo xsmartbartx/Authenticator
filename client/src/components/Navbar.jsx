14469bbae19bf35f45c15ae6c6562ce21754f308
xsmartbartx <bw141105@gmail.com>
2025-10-16 21:01:11 +0200

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
-        <img src={assets.logo} alt="" className='W-28 SM:W-32'></img>
-        <button>Logowanie</button>
+    <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24
+    absolute top-0'>
+
+      <img src={assets.logo} alt="" className='W-28 SM:W-32'></img>
+
+      <button className='flex items-center gap-2 border border-gray-500 rounded-full
+      px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Logowanie <img src={assets.arrow_icon} alt="" /></button>
     </div>
   )
 }
