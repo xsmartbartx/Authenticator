cded75a2a989ad0f7fdb94214fc325e577ca0f3d
xsmartbartx <bw141105@gmail.com>
2025-10-16 21:16:14 +0200

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div>
         <img src={assets.header_img} alt="" className='w-36 h-36 roinded-full mb-6'/>
-        <h1>Witaj<img className='w-8 aspect-square' src={assets.hand_wave} alt=""/></h1>
+        <h1>Cześć<img className='w-8 aspect-square' src={assets.hand_wave} alt=""/></h1>
+        <h2>Witam w mojej aplikacji</h2>
+        <p></p>
     </div>
   )
 }
