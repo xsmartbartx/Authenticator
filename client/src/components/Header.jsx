e657106624b6a755ab26abec49d8f891abbcdb84
xsmartbartx <bw141105@gmail.com>
2025-10-16 21:26:21 +0200

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,11 +3,12 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex-col items-center justify-center text-center mt-32 mb-16'>
         <img src={assets.header_img} alt="" className='w-36 h-36 roinded-full mb-6'/>
-        <h1>Cześć<img className='w-8 aspect-square' src={assets.hand_wave} alt=""/></h1>
+        <h1 className='flex items-center gap-2 text-x2 sm:text-3x1 font-medium mb-2'>Cześć<img className='w-8 aspect-square' src={assets.hand_wave} alt=""/></h1>
         <h2>Witam w mojej aplikacji</h2>
         <p></p>
+        <button>Zacznij</button>
     </div>
   )
 }
