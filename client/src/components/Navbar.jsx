b17a9d0c3f86f86cb3219e19bc155371fa8443ea
xsmartbartx <bw141105@gmail.com>
2025-10-16 22:29:44 +0200

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { use } from 'react'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
+
+    const navigate - useNavigate();
+
   return (
     <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24
     absolute top-0'>
 
       <img src={assets.logo} alt="" className='W-28 SM:W-32'></img>
 
-      <button className='flex items-center gap-2 border border-gray-500 rounded-full
+      <button onClick={()=>navigate('/login') }
+      className='flex items-center gap-2 border border-gray-500 rounded-full
       px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Logowanie <img src={assets.arrow_icon} alt="" /></button>
     </div>
   )
