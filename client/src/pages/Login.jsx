bc16f50058a523906a59bf73b1faca321495a601
xsmartbartx <bw141105@gmail.com>
2025-10-23 21:04:11 +0200

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,6 +24,21 @@ const Login = () => {
             <img src={assets.person_icon} alt="" />
             <input className='bg-transparent outline-none' type="text" placeholder="Username" required/>
           </div>
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+          rounded-full bg-[#333A5C]'>
+            <img src={assets.person_icon} alt="" />
+            <input className='bg-transparent outline-none' type="email" placeholder="Email id" required/>
+          </div>
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+          rounded-full bg-[#333A5C]'>
+            <img src={assets.person_icon} alt="" />
+            <input className='bg-transparent outline-none' type="text" placeholder="Username" required/>
+          </div>
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+          rounded-full bg-[#333A5C]'>
+            <img src={assets.person_icon} alt="" />
+            <input className='bg-transparent outline-none' type="text" placeholder="Username" required/>
+          </div>
         </form>
 
        </div>
