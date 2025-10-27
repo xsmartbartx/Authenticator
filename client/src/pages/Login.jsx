2c37189d7294b644d6358219e7b7b5aaf721e3c2
xsmartbartx <bw141105@gmail.com>
2025-10-27 19:49:39 +0100

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,13 +19,13 @@ const Login = () => {
         'Create your account' : 'Login to your account!'}</p>
 
         <form>
-          {state === 'Sign Up' && (}
+          {state === 'Sign Up'  (}
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
           rounded-full bg-[#333A5C]'>
             <img src={assets.person_icon} alt="" />
             <input className='bg-transparent outline-none' type="text" placeholder="Username" required/>
           </div>
-          )}
+          
 
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
           rounded-full bg-[#333A5C]'>
@@ -46,9 +46,10 @@ const Login = () => {
 
            {state === 'Sign Up' ? (<p className='text-gray-400 text-center text-xs mt-4'>Masz już konto?{' '}
             <span className='text-blue-400 cursor-pointer underline'>Zaloguj się</span></p>) : 
+            (<p className='text-gray-400 text-center text-xs mt-4'>Nie masz konta?{' '}
+            <span className='text-blue-400 cursor-pointer underline'>Zarejestruj się</span></p>)}
 
-          <p className='text-gray-400 text-center text-xs mt-4'>Nie masz konta?{' '}
-            <span className='text-blue-400 cursor-pointer underline'>Zarejestruj się</span></p>
+          
        </div>
     </div>
   )
