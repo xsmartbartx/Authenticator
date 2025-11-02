373ff5ab91513f19aec62078d002cc36efd7857b
xsmartbartx <bw141105@gmail.com>
2025-11-02 23:42:42 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
+import { ToastContainer } from 'react-toastify'
+  import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
