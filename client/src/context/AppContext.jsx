8a756865282a671213115e923ab246d626887f88
xsmartbartx <bw141105@gmail.com>
2025-11-02 22:22:34 +0100

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,8 +4,12 @@ export const AppContext = createContext(null);
 
 export const AppContextProvider = ({ props }) => {
 
-    const value = {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [user, setUser] = useState(false)
 
+    const value = {
+        backendUrl
     }
 
     return (
