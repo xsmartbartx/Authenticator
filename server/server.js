3b88f6889f83702d331358d74643e6330a3e2fe5
xsmartbartx <bw141105@gmail.com>
2025-11-12 19:29:53 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 4000
 connectDB();
 
+const allowedOrigins = ['http://localhost:5173'];
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
