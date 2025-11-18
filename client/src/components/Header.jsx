6ff435b4bef39748e748d6f221483efb0190288d
xsmartbartx <bw141105@gmail.com>
2025-11-18 17:21:56 +0100

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
+import { AppContent } from '../context/AppContext'
 
 const Header = () => {
+
+    const {userData} = useContext(AppContent)
+
   return (
     <div className='flex-col items-center justify-center text-center mt-32 mb-16'>
         <img src={assets.header_img} alt="" className='w-36 h-36 roinded-full mb-6'/>
 
         <h1 className='flex items-center gap-2 text-x2 sm:text-3x1 font-medium mb-2'>
-            Cześć<img className='w-8 aspect-square' src={assets.hand_wave} alt=""/></h1>
+            Cześć {userData ? userData.name : 'Developer'}<img className='w-8 aspect-square' src={assets.hand_wave} alt=""/></h1>
 
         <h2 className='text-3x1 sm:text-5x1 font-semibold mb-4'>Witam w mojej aplikacji</h2>
 
