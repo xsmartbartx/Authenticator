b17622421f6f106f6d78c6ee05034ebe07d4ce13
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-02 20:52:48 +0100

    Update EmailVerify.jsx

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const EmailVerify = () => {
   return (
-    <div>Weryfikacja Email</div>
+    <div className='flex items-center justify-center min-h-screen bg-gradient-to-br
+     from-blue-200 to-purple-400'>
+      <img onClick={()=>Navigate('/')} src={assets.logo} alt="" className='absolute
+       left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+     </div>
   )
 }
 
