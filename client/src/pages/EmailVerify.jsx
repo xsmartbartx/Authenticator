68ab82157925e7e1e7c6a7c65f75e21a83fd33ab
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-07 19:58:45 +0100

    Update EmailVerify.jsx

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -8,8 +8,16 @@ const EmailVerify = () => {
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute
        left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
        <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
-        <h1 className='text-white text-2x1 font-semibold text-center mb-4'>Zweryfikuj Email OTP</h1>
-        <p className='text-center mb-6 text-indigo-300'>Wprowadź 6-cyfrowy kod wysłany na adres email</p>
+        <h1 className='text-white text-2x1 font-semibold text-center mb-4'>
+          Zweryfikuj Email OTP</h1>
+        <p className='text-center mb-6 text-indigo-300'>
+          Wprowadź 6-cyfrowy kod wysłany na adres email</p>
+        <div className='flex justify-between mb-8'>
+          {Array(6).fill(0).map((_, index) => {
+            <input type='text'/>
+          })}
+
+        </div>
        </form>
      </div>
   )
