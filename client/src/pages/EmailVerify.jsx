2e1121fe79fd2ba082db1685cf519e1c81de0937
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-07 20:01:40 +0100

    Update EmailVerify.jsx

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -14,7 +14,8 @@ const EmailVerify = () => {
           Wprowadź 6-cyfrowy kod wysłany na adres email</p>
         <div className='flex justify-between mb-8'>
           {Array(6).fill(0).map((_, index) => {
-            <input type='text' maxLength='1' key={index}/>
+            <input type='text' maxLength='1' key={index} required
+            className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'/>
           })}
 
         </div>
