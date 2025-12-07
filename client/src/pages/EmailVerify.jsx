8aa82ea0748b15351a52101f51bc1b3b03ebee8a
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-07 19:39:20 +0100

    Update EmailVerify.jsx

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -5,7 +5,7 @@ const EmailVerify = () => {
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br
      from-blue-200 to-purple-400'>
-      <img onClick={()=>Navigate('/')} src={assets.logo} alt="" className='absolute
+      <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute
        left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
        <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
         <h1 className='text-white text-2x1 font-semibold text-center mb-4'>Zweryfikuj Email OTP</h1>
