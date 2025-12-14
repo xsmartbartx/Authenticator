be68119d1172a99de291da52c03165c2383ee878
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-15 00:14:15 +0100

    Update EmailVerify.jsx

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -61,7 +61,7 @@ const EmailVerify = () => {
      from-blue-200 to-purple-400'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute
        left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
-       <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+       <form onSubmit={onSubmitHandler} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
         <h1 className='text-white text-2x1 font-semibold text-center mb-4'>
           Zweryfikuj Email OTP</h1>
         <p className='text-center mb-6 text-indigo-300'>
