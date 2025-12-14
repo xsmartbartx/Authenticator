45668a3d7fd30277b0831e13ac57bb9f1b9394b7
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-15 00:19:57 +0100

    Update ResetPassword.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const ResetPassword = () => {
   return (
-    <div>ResetPassword</div>
+    <div className='flex items-center justify-center min-h-screen bg-gradient-to-br
+     from-blue-200 to-purple-400'></div>
   )
 }
 
