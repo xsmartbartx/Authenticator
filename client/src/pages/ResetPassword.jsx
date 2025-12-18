4e8dd2e1852bf24fb93e5e380a2d39005fc23b0c
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-18 14:53:10 +0100

    Update ResetPassword.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
 const ResetPassword = () => {
 
   const navigate = useNavigate();
-  const [email, setEmail] = useNavigate('');
+  const [email, setEmail] = useState('');
 
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br
@@ -29,6 +29,28 @@ const ResetPassword = () => {
         <button className='w-full py-2.5 bg-gradient-to-r from-indigo-500
          to-indigo-900 text-white rounded-full mt-3'></button>
       </form>
+
+{/* form wprowadzenia otp */}
+
+<form onSubmit={onSubmitHandler} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+        <h1 className='text-white text-2x1 font-semibold text-center mb-4'>
+          Zweryfikuj Email OTP</h1>
+        <p className='text-center mb-6 text-indigo-300'>
+          Wprowadź 6-cyfrowy kod wysłany na adres email</p>
+        <div className='flex justify-between mb-8' onPaste={handlePaste}>
+          {Array(6).fill(0).map((_, index) =>(
+            <input type="text" maxLength='1' key={index} required
+            className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl
+             rounded-md'
+             ref={e => inputRefs.current[index] = e}
+             onInput={(e) => handleInput(e, index) }
+             onKeyDown{(e) => handleKeyDown(e, index)}
+             />
+          ))}
+        </div>
+        <button className='w-full py-3 bg-gradient-to-r from-indigo-500
+        to-indigo-900 text-white rounded-full'>Zweryfikuj email</button>
+       </form>
      </div>
   )
 }
