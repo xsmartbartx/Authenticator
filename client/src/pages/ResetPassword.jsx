74da09804910d5b9a6faca98253aa02b19c55f16
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-18 14:19:23 +0100

    Update ResetPassword.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -11,6 +11,18 @@ const ResetPassword = () => {
      from-blue-200 to-purple-400'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute
         left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+
+{/* wprowadz email id */}
+      <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+        <h1 className='text-white text-2xl font-semibold text-center mb-4'>Zresetuj
+        hasło</h1>
+        <p className='text-center mb-6 text-indigo-300'>Wprowadź zarejestrowany adres
+        email</p>
+        <div className=''>
+          <img src="" alt="" />
+          <input type="email" />
+        </div>
+      </form>
      </div>
   )
 }
