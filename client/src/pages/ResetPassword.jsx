c8e9903c8492eacd44335523972315f86d020ef5
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-18 14:04:02 +0100

    Update ResetPassword.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const ResetPassword = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br
-     from-blue-200 to-purple-400'></div>
+     from-blue-200 to-purple-400'>
+      <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute
+        left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+     </div>
   )
 }
 
