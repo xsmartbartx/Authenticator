a0a73d2ad27b98e18aea9aa4b256c8b662a54f84
xsmartbartx <73461850+xsmartbartx@users.noreply.github.com>
2025-12-18 14:27:11 +0100

    Update ResetPassword.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -21,7 +21,8 @@ const ResetPassword = () => {
         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
         rounded-full bg-[#333A5C]'>
           <img src={assets.email_icon} alt="" className='w-3 h-3' />
-          <input type="email" placeholder='' />
+          <input type="email" placeholder='Email id'
+          className='bg-transparent outline-none'/>
         </div>
       </form>
      </div>
